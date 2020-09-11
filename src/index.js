f770260826be0a420e5ef6bbb5854f049a4cef3a
Hugo <>
2020-09-11 18:04:37 +0800

    :arrow_up: Upgrade postcss-sparrow-units-filter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import * as R from 'ramda'
+import * as S from 'sanctuary'
 
-export default (options) => (decl) => {
-
+export default (config) => (decl) => {
+  const options = {
+    scale: config.scale,
+    changeRate: config.changeRate
+  }
 }
+
+// const options = {
+//   props: R.defaultTo(['display'])(getProps(config)),
+//   values: R.defaultTo(['none'])(getValues(config)),
+//   declValue: R.defaultTo('auto')(getDeclValue(config))
+// }
