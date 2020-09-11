afa5950f2b23bed06d764ee17a3c6a45b346daf3
Hugo <>
2020-09-11 18:12:46 +0800

    :white_check_mark: Add description for test

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,3 +4,38 @@ const R = require('ramda')
 const chai = require('chai')
 const sinon = require('sinon')
 const expect = chai.expect
+
+describe('Be water', function () {
+  let css, beforeDeclCount
+
+  beforeEach(function () {
+    css = `
+    body{
+      padding: 5rem;
+      display: none;
+      font-family: san-serif;
+    }
+
+    a{
+      letter-spacing: 20px;
+      line-height: 3;
+      color: #be132d;
+      border: 2px solid #f5f5f5;
+    }`
+
+    const beforeTransformation = postcss
+      .parse(css, {
+        from: undefined
+      })
+  })
+
+  afterEach(function () {
+    sinon.restore()
+  })
+
+  describe('when the value of the filtered rule has unit', function () {
+    it('should transform it with clamp()', function () {
+
+    })
+  })
+})
